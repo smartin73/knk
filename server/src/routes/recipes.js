2f437a49cab2d4b322d5f93a0ca9532ef47955d8
smartin73 <smartin73@gmail.com>
2026-03-11 08:20:35 -0400

    fix: correct typo in query for fetching recipe steps by recipe_id

--- server/src/routes/recipes.js
+++ server/src/routes/recipes.js
@@ -76,7 +76,7 @@ router.get('/', async (req, res) => {
 router.get('/:id', async (req, res) => {
   const [recipe, steps, ingredients] = await Promise.all([
     query('SELECT * FROM recipes WHERE id = $1', [req.params.id]),
-    qquery('SELECT *, step_time::text as step_time FROM recipe_steps WHERE recipe_id = $1 ORDER BY step_number', [req.params.id]),
+    query('SELECT *, step_time::text as step_time FROM recipe_steps WHERE recipe_id = $1 ORDER BY step_number', [req.params.id]),
     query(`SELECT ri.*, ii.item_name as ingredient_item_name, ii.cost_per_gram
            FROM recipe_ingredients ri
            LEFT JOIN ingredient_items ii ON ri.ingredient_id = ii.id
